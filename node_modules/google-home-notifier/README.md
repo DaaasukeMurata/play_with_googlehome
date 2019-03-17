6f32bfbee3d1caaa833769cc1e4bb012b28d4dd5
DaaasukeMurata <daaaaaasuke@gmail.com>
2019-03-17 15:52:37 +0900

    [add] add google-home-notifier to git management

---
+++ node_modules/google-home-notifier/README.md
@@ -0,0 +1,67 @@
+# google-home-notifier
+Send notifications to Google Home
+
+#### Installation
+```sh
+$ npm install google-home-notifier
+```
+
+#### Usage
+```javascript
+var googlehome = require('google-home-notifier');
+
+googlehome.device('Google Home'); // Change to your Google Home name
+// or if you know your Google Home IP
+// googlehome.ip('192.168.x.x');
+googlehome.accent('us'); // optional: 'us'= american voice (default), 'uk'= british voice
+googlehome.notify('Hey Foo', function(res) {
+  console.log(res);
+});
+```
+
+#### Listener
+If you want to run a listener, take a look at the example.js file. You can run this from a Raspberry Pi, pc or mac. The example uses ngrok so the server can be reached from outside your network. I tested with ifttt.com Maker channel and it worked like a charm.
+
+```sh
+$ git clone https://github.com/noelportugal/google-home-notifier
+$ cd google-home-notifier
+$ npm install
+$ node example.js
+POST "text=Hello Google Home" to:
+    http://localhost:8080/google-home-notifier
+    https://xxxxx.ngrok.io/google-home-notifier
+example:
+curl -X POST -d "text=Hello Google Home" https://xxxxx.ngrok.io/google-home-notifier
+```
+#### Raspberry Pi
+If you are running from Raspberry Pi make sure you have the following before nunning "npm install":
+Use the latest nodejs dist.
+```sh
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install nodejs
+```
+Also install these packages:
+```sh
+sudo apt-get install git-core libnss-mdns libavahi-compat-libdnssd-dev
+```
+
+## After "npm install"
+
+Modify the following file "node_modules/mdns/lib/browser.js"
+```sh
+vi node_modules/mdns/lib/browser.js
+```
+Find this line:
+```javascript
+Browser.defaultResolverSequence = [
+  rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in dns_sd ? rst.DNSServiceGetAddrInfo() : rst.getaddrinfo()
+, rst.makeAddressesUnique()
+];
+```
+And change to:
+```javascript
+Browser.defaultResolverSequence = [
+  rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in dns_sd ? rst.DNSServiceGetAddrInfo() : rst.getaddrinfo({families:[4]})
+, rst.makeAddressesUnique()
+];
+```
---
+++ node_modules/google-home-notifier/google-home-notifier.js
@@ -0,0 +1,110 @@
+var Client = require('castv2-client').Client;
+var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+var mdns = require('mdns');
+var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+var deviceAddress;
+var language;
+
+var device = function(name, lang = 'en') {
+    device = name;
+    language = lang;
+    return this;
+};
+
+var ip = function(ip) {
+  deviceAddress = ip;
+  return this;
+}
+
+var googletts = require('google-tts-api');
+var googlettsaccent = 'us';
+var accent = function(accent) {
+  googlettsaccent = accent;
+  return this;
+}
+
+var notify = function(message, callback) {
+  if (!deviceAddress){
+    browser.start();
+    browser.on('serviceUp', function(service) {
+      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+      if (service.name.includes(device.replace(' ', '-'))){
+        deviceAddress = service.addresses[0];
+        getSpeechUrl(message, deviceAddress, function(res) {
+          callback(res);
+        });
+      }
+      browser.stop();
+    });
+  }else {
+    getSpeechUrl(message, deviceAddress, function(res) {
+      callback(res);
+    });
+  }
+};
+
+var play = function(mp3_url, callback) {
+  if (!deviceAddress){
+    browser.start();
+    browser.on('serviceUp', function(service) {
+      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+      if (service.name.includes(device.replace(' ', '-'))){
+        deviceAddress = service.addresses[0];
+        getPlayUrl(mp3_url, deviceAddress, function(res) {
+          callback(res);
+        });
+      }
+      browser.stop();
+    });
+  }else {
+    getPlayUrl(mp3_url, deviceAddress, function(res) {
+      callback(res);
+    });
+  }
+};
+
+var getSpeechUrl = function(text, host, callback) {
+  googletts(text, language, 1).then(function (url) {
+    onDeviceUp(host, url, function(res){
+      callback(res)
+    });
+  }).catch(function (err) {
+    console.error(err.stack);
+  });
+};
+
+var getPlayUrl = function(url, host, callback) {
+    onDeviceUp(host, url, function(res){
+      callback(res)
+    });
+};
+
+var onDeviceUp = function(host, url, callback) {
+  var client = new Client();
+  client.connect(host, function() {
+    client.launch(DefaultMediaReceiver, function(err, player) {
+
+      var media = {
+        contentId: url,
+        contentType: 'audio/mp3',
+        streamType: 'BUFFERED' // or LIVE
+      };
+      player.load(media, { autoplay: true }, function(err, status) {
+        client.close();
+        callback('Device notified');
+      });
+    });
+  });
+
+  client.on('error', function(err) {
+    console.log('Error: %s', err.message);
+    client.close();
+    callback('error');
+  });
+};
+
+exports.ip = ip;
+exports.device = device;
+exports.accent = accent;
+exports.notify = notify;
+exports.play = play;
